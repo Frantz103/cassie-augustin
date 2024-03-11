2eb31c436495871d8118246f9c716accb2f90b48
frantz <hirefrantz@gmail.com>
2024-03-11 11:26:25 -0700

    modified cypress/e2e/basic.cy.ts, index.astro, base.css, netlify.toml, package.json, package-lock.json

--- cypress/e2e/basic.cy.ts
+++ cypress/e2e/basic.cy.ts
@@ -4,14 +4,14 @@ describe('empty spec', () => {
   })
 
   it('displays the resources text', () => {
-    cy.get('h1')
-    .contains('Quickstart Template');
+    cy.get('h1').contains('Ludnie Cassie Augustin');
   })
-  it('renders the image', () => {
-    cy.get('img')
-    .should('be.visible')
-    .and(($img) => {
-      expect($img[0].naturalWidth).to.be.greaterThan(0);
-    })
-  })
+
+  // it('renders the image', () => {
+  //   cy.get('img')
+  //     .should('be.visible')
+  //     .and(($img) => {
+  //       expect($img[0].naturalWidth).to.be.greaterThan(0);
+  //     })
+  // })
 })
